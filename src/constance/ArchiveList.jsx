a53cb6cb7789bad34374dfb15b0a35f6660e777b
Shariful Islam <sajukhan12905@gmail.com>
2024-07-31 15:12:53 +0600

    Handling the error

--- src/constance/ArchiveList.jsx
+++ src/constance/ArchiveList.jsx
@@ -3,168 +3,168 @@ export const projectArray = [
     title: "Modern Ecommerce Website",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "React Ecommerce Website",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "React Ecommerce Website",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "Amazon clone with login ",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "React Ecommerce Website",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "Modern Ecommerce Website",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "Amazon clone with login ",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "Modern Ecommerce Website",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "React Ecommerce Website",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "Amazon clone with login ",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "Modern Ecommerce Website",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "Amazon clone with login ",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "Modern Ecommerce Website",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "Amazon clone with login ",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "Modern Ecommerce Website",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "React Ecommerce Website",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "Amazon clone with login ",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "Modern Ecommerce Website",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "Amazon clone with login ",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "Modern Ecommerce Website",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "React Ecommerce Website",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "Amazon clone with login ",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "Modern Ecommerce Website",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
   {
     title: "Amazon clone with login ",
     description:
       "Some description will go here. I make this project alone. I use some Advanced Tech for making it.",
-    listItem: ["Reactjs", "TailwindCass", "JavaScript"],
+    listItem: ["Reactjs", "TailWindCss", "JavaScript"],
     link: "#",
   },
 ];
